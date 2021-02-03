5a38bd314ca36de6fa52aa27d52585b3a7c605d6
jwatts82 <jessperr182@hotmail.com>
2021-02-03 15:40:56 -0800

    begin browser routing

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import thunk from 'redux-thunk'
 import rootReducer from './reducers/indexReducer'
 import { composeWithDevTools } from 'redux-devtools-extension'
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+import { BrowserRouter as Router } from 'react-router-dom';
 
 const store=createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)))
 
@@ -16,7 +16,10 @@ const store=createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk))
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <Router>
+      <App />
+    </Router>
+   
   </Provider>,
   document.getElementById('root')
 );
