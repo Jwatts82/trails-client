f764c6f244d88e4e9716c51091c8205e39e1b3df
jwatts82 <jessperr182@hotmail.com>
2021-02-03 20:17:49 -0800

    add form

--- src/components/ParkForm.js
+++ src/components/ParkForm.js
@@ -1,11 +1,64 @@
 import React, { Component } from 'react'
+import { addPark } from '../actions/parks'
+import { connect } from 'react-redux'
 
-export default class ParkForm extends Component {
+class ParkForm extends Component {
+    state = {
+        park: {
+            name: "",
+            location: "",
+        },
+        loading: false
+    }
+
+    handleChange = event => {
+        this.setState({...this.state,
+            park: {...this.state.park,
+                 [event.target.name]: event.target.value
+            },
+
+            })
+    }
+
+    handleOnSubmit = event => {
+        event.preventDefault()
+        const park = {...this.state.park}
+        console.log(park)
+        this.props.addPark(park)
+        this.setState({
+            park: {
+                name: "",
+                location: "",
+            },
+            loading: false
+        })
+
+    }
+
     render() {
         return (
-            <div>
-                <h1>i will be a from</h1>
-            </div>
-        )
+          
+            <form onSubmit={this.handleOnSubmit}>
+                <label>Park Name:</label>
+                <input 
+                name="name"
+                type="text"
+                value={this.state.park.name}
+                onChange={this.handleChange}/> 
+                <br/>
+                <label>Location:</label>
+
+                <input 
+                name="location"
+                type="text"
+                value={this.state.park.location}
+                onChange={this.handleChange}/> <br/>
+
+                <button type="submit">Add Park</button>       
+            </form>
+        );
+      }
     }
-}
+
+
+export default connect(null, { addPark })(ParkForm);
