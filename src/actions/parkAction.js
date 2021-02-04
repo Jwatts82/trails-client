1409938fd73f16d9b291d876fdf2f0a590fccc75
jwatts82 <jessperr182@hotmail.com>
2021-02-03 17:17:19 -0800

    add actions

---
+++ src/actions/parkAction.js
@@ -0,0 +1,23 @@
+export const getParks = () => {
+    return (dispatch) => {
+        dispatch({type: "LOADING_PARKS"})
+        fetch('http://localhost:3001/parks')
+        .then(res => res.json())
+        .then(parks => dispatch({type: "FETCH_PARKS", payload: parks }))
+    }
+}
+
+export const addPark = park => {
+    return(dispatch) => {
+        dispatch({type: "ADD_PARK"})
+        fetch('http://localhost:3001/parks', {
+            method: 'POST',
+            body: JSON.stringify(park),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+        .then(res => res.json())
+        .then(park => dispatch({type: "PARK_ADDED", payload: park}))
+    }
+}
