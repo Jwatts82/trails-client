cde1493a8263aff1fa8ca7c06e48497840e281f0
jwatts82 <jessperr182@hotmail.com>
2021-02-05 17:47:12 -0800

    add nav bar styling

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,58 +1,80 @@
 import React from 'react'
-import { NavLink } from 'react-router-dom'
-
-const link = {
-    width: '100px',
-    padding: '12px',
-    margin: '0 6px 6px',
-    background: 'blue',
-    textDecoration: 'none',
-    color: 'white'
-}
+//import { NavLink } from 'react-router-dom'
+import { Navbar, Nav } from 'react-bootstrap';
 
 const Navigation = () => {
     return (
-        <div>
-            <NavLink 
-                to='/'
-                exact
-                style= {link}
-                activestyle={{
-                    background: 'darkblue'
-                }}>
-            Home</NavLink>
-
-            <NavLink 
-                to='/parks'
-                exact
-                style= {link}
-                activestyle={{
-                    background: 'darkblue'
-                }}>
-            All Parks</NavLink>
-
-            <NavLink 
-                to='/parks/new'
-                exact
-                style= {link}
-                activestyle={{
-                    background: 'darkblue'
-                }}>
-            Add Park</NavLink>
-
-            <NavLink 
-                to='/trails'
-                exact
-                style= {link}
-                activestyle={{
-                    background: 'darkblue'
-                }}>
-            All Trails</NavLink>
-            
-
+        <div className="Navigation">
+            <>
+            <Navbar bg="dark" variant="dark">
+                <Navbar.Brand href="/">Trails</Navbar.Brand>
+                <Nav className="mr-auto">
+                    <Nav.Link href='/parks'>All Parks</Nav.Link>
+                    <Nav.Link href="/parks/new">Add Park</Nav.Link>
+                    <Nav.Link href="/trails">All Trails</Nav.Link>
+                </Nav>
+            </Navbar>
+            </>
         </div>
     )
 }
 
+// const link = {
+//     width: '100px',
+//     padding: '12px',
+//     margin: '0 6px 6px',
+//     background: 'blue',
+//     textDecoration: 'none',
+//     color: 'white'
+// }
+
+// const Navigation = () => {
+//     return (
+//         <div>
+//             <NavLink 
+//                 to='/'
+//                 exact
+//                 style= {link}
+//                 activestyle={{
+//                     background: 'darkblue'
+//                 }}>
+//             Home</NavLink>
+
+//             <NavLink 
+//                 to='/parks'
+//                 exact
+//                 style= {link}
+//                 activestyle={{
+//                     background: 'darkblue'
+//                 }}>
+//             All Parks</NavLink>
+
+//             <NavLink 
+//                 to='/parks/new'
+//                 exact
+//                 style= {link}
+//                 activestyle={{
+//                     background: 'darkblue'
+//                 }}>
+//             Add Park</NavLink>
+
+//             <NavLink 
+//                 to='/trails'
+//                 exact
+//                 style= {link}
+//                 activestyle={{
+//                     background: 'darkblue'
+//                 }}>
+//             All Trails</NavLink>
+            
+
+//         </div>
+//     )
+// }
+
 
 export default Navigation;
+
+
+
+
--- src/containers/ParksContainer.js
+++ src/containers/ParksContainer.js
@@ -1,15 +1,16 @@
-import React, { Component } from 'react'
-import ParkForm from '../components/ParkForm'
+//import React, { Component } from 'react'
+//import ParkForm from '../components/ParkForm'
 
-class ParksContainer extends Component {
-    render() {
+//class ParksContainer extends Component {
+  //  render() {
         
-        return (
-            <div className="parksContainer">
-                <ParkForm />
-            </div>
-        )
-    }
-}
+    //    return (
+      //      <div className="parksContainer">
+        //        <ParkForm />
+          //  </div>
+        //)
+    //}
+//}
 
-export default ParksContainer;
+//export default ParksContainer;
+///////////////////////////////////////////
